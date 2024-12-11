3c5242a1ffcf282a5fa0ed7ea6eb2b0ac029a0d6
Darliane <140345464+darlianecunha@users.noreply.github.com>
2024-12-11 16:09:54 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -30,163 +30,146 @@ def plot_radar_chart(scores, categories):
 # Definir variáveis agrupadas por ODS com prefixos
 
 variables = {
-    "SDG 7": [
-        {
-            "name": "7.1 Number of awareness programs on rational energy use",
-            "options": [
-                "0: No program in operation.",
-                "1: Up to 2 programs in operation, reaching up to 100 people.",
-                "2: 3-5 programs in operation, reaching 101-500 people.",
-                "3: More than 5 programs in operation, reaching over 500 people."
-            ]
-        },
-        {
-            "name": "7.2 Number of energy efficiency management programs",
-            "options": [
-                "0: No program in operation.",
-                "1: At least one program, resulting in up to 5% reduction in energy consumption.",
-                "2: At least one program, resulting in a reduction greater than 5% and less than or equal to 10% in energy consumption.",
-                "3: At least one program with energy consumption reductions exceeding 10%."
-            ]
-        },
-        {
-            "name": "7.3 Number of technological innovation initiatives in energy efficiency",
-            "options": [
-                "0: No initiative.",
-                "1: Up to 2 technological initiatives implemented.",
-                "2: 3-5 technological initiatives implemented.",
-                "3: More than 5 initiatives implemented."
-            ]
-        },
-        {
-            "name": "7.4 Percentage of renewable energy contracted and produced in port facilities",
-            "options": [
-                "0: No use of renewable energy.",
-                "1: Up to 10% of the energy used is renewable.",
-                "2: More than 10% up to 50% of the energy used is renewable.",
-                "3: More than 50% of the energy used is renewable."
-            ]
-        },
-        {
-            "name": "7.5 Percentage of biofuels in electrical and mechanical loads",
-            "options": [
-                "0: No use of biofuels.",
-                "1: Less than 5% of the loads are operated with biofuels.",
-                "2: 5% to 20% of the loads are operated with biofuels.",
-                "3: More than 20% of the loads are operated with biofuels."
-            ]
-        },
-        {
-            "name": "7.6 Number of technological innovation initiatives in renewable energy",
-            "options": [
-                "0: No initiative.",
-                "1: Up to 2 initiatives in the planning or pilot phase.",
-                "2: 3-5 initiatives in the implementation phase with preliminary results.",
-                "3: More than 5 initiatives in full operation with proven results."
-            ]
-        },
-        {
-            "name": "7.7 Diversity of renewable energy sources in port facilities",
-            "options": [
-                "0: No use of renewable sources.",
-                "1: Use of 1 different type of renewable energy.",
-                "2: Use of 2 different types of renewable energy.",
-                "3: Use of 3 or more different types of renewable energy."
-            ]
-        },
-        {
-            "name": "7.8 Number of partnerships for the promotion of clean energy",
-            "options": [
-                "0: No partnership established.",
-                "1: Up to 2 partnerships established with a focus on clean energy.",
-                "2: 3-5 partnerships established with a focus on clean energy.",
-                "3: More than 5 partnerships established with a focus on clean energy."
-            ]
-        },
-        {
-            "name": "7.9 Number of charging stations for electric vehicles",
-            "options": [
-                "0: No charging station available.",
-                "1: Up to 5 charging stations available.",
-                "2: 6-15 charging stations available.",
-                "3: More than 15 charging stations available."
-        ]}
+"ODS 7": [
+{"name": "7.1 Número de programas de conscientização em uso racional de energia", "options": [
+"0: Nenhum programa em operação.",
+"1: Até 2 programas em operação, atingindo até 100 pessoas.",
+"2: 3-5 programas em operação, atingindo 101-500 pessoas.",
+"3: Mais de 5 programas em operação, atingindo mais de 500 pessoas."
+]},
+{"name": "7.2 Número de programas de gestão de eficiência energética", "options": [
+"0: Nenhum programa em operação.",
+"1: Pelo menos um programa, resultando em até 5% de redução no consumo de energia.",
+"2: Pelo menos um programa, resultando em uma redução maior que 5% e menor ou igual a 10% no consumo de energia.",
+"3: Pelo menos um programa com reduções no consumo de energia superiores a 10%."
+]},
+{"name": "7.3 Número de iniciativas de inovação tecnológica em eficiência energética", "options": [
+"0: Nenhuma iniciativa.",
+"1: Até 2 iniciativas tecnológicas implementadas.",
+"2: 3-5 iniciativas tecnológicas implementadas.",
+"3: Mais de 5 iniciativas implementadas."
+]},
+{"name": "7.4 Percentual de energia renovável contratada e produzida em instalações portuárias", "options": [
+"0: Nenhum uso de energia renovável.",
+"1: Até 10% da energia utilizada é renovável.",
+"2: Mais de 10% até 50% da energia usada é renovável.",
+"3: Mais de 50% da energia usada é renovável."
+]},
+{"name": "7.5 Percentual de biocombustíveis em cargas elétricas e mecânicas", "options": [
+"0: Nenhum uso de biocombustíveis.",
+"1: Menos de 5% das cargas são operadas com biocombustíveis.",
+"2: 5% a 20% das cargas são operadas com biocombustíveis.",
+"3: Mais de 20% das cargas são operadas com biocombustíveis."
+]},
+{"name": "7.6 Número de iniciativas de inovação tecnológica em energia renovável", "options": [
+"0: Nenhuma iniciativa.",
+"1: Até 2 iniciativas na fase de planejamento ou piloto.",
+"2: 3-5 iniciativas na fase de implementação com resultados preliminares.",
+"3: Mais de 5 iniciativas em plena operação com resultados comprovados."
+]},
+{"name": "7.7. Diversidade de fontes de energia renovável em instalações portuárias", "options": [
+"0: Nenhum uso de fontes renováveis.",
+"1: Uso de 1 tipo diferente de energia renovável.",
+"2: Uso de 2 tipos diferentes de energia renovável.",
+"3: Uso de 3 ou mais tipos diferentes de energia renovável."
+]},
+{"name": "7.8 Quantidade de parcerias para promoção de energia limpa", "options": [
+"0: Nenhuma parceria estabelecida.",
+"1: Até 2 parcerias estabelecidas com foco em energia limpa.",
+"2: 3-5 parcerias estabelecidas com foco em energia limpa.",
+"3: Mais de 5 parcerias estabelecidas com foco em energia limpa."
+]},
+{"name": "7.9 Quantidade de estações de carregamento para veículos elétricos", "options": [
+"0: Nenhuma estação de carregamento disponível.",
+"1: Até 5 estações de carregamento disponíveis.",
+"2: 6-15 estações de carregamento disponíveis.",
+"3: Mais de 15 estações de carregamento disponíveis."
+]},
+{"name": "7.10 Percentual de fornecimento de energia renovável para navios", "options": [
+"0: Nenhum fornecimento de energia renovável para navios.",
+"1: Menos de 10% do fornecimento de energia para navios vem de fontes renováveis.",
+"2: Entre 10% e 30% do fornecimento de energia para navios vem de fontes renováveis.",
+"3: Mais de 30% do fornecimento de energia para navios vem de fontes renováveis."
+]},
+{"name": "7.11 Percentual de abastecimento com GNL", "options": [
+"0: Nenhum abastecimento de GNL.",
+"1: Menos de 5% do fornecimento total de combustível é com GNL.",
+"2: Entre 5% e 20% do fornecimento total de combustível é com GNL.",
+"3: Mais de 20% do fornecimento total de combustível é com GNL."
+]},
+{"name": "7.12 Número de iniciativas de inovação tecnológica em atendimento elétrico e energético para navios", "options": [
+"0: Nenhuma iniciativa.",
+"1: Até 2 iniciativas de inovação tecnológica em fase de planejamento ou piloto.",
+"2: 3-5 iniciativas em fase de implementação ou avaliação de eficácia.",
+"3: Mais de 5 iniciativas em plena operação, com resultados de eficácia comprovados."
+]},
+{"name": "7.13 Quantidade de navios usando energia renovável no porto", "options": [
+"0: Nenhum navio utiliza energia renovável.",
+"1: Até 5% dos navios no porto utilizam energia renovável.",
+"2: 5% a 15% dos navios no porto utilizam energia renovável.",
+"3: Mais de 15% dos navios no porto utilizam energia renovável."
+]},
+{"name": "7.14 Tarifas diferenciadas para navios com desempenho acima dos padrões ambientais", "options": [
+"0: Nenhuma aplicação de tarifas diferenciadas.",
+"1: Até 5% de desconto nas tarifas portuárias.",
+"2: 5% a 15% de desconto nas tarifas portuárias.",
+"3: Mais de 15% de desconto nas tarifas portuárias."
+]}
+
 ],
 
-"SDG 13": [
-        {
-            "name": "13.1 Status of Climate Change Strategy Plan",
-            "options": [
-                "0: No strategies.",
-                "1: Strategy plan under development, no actions implemented.",
-                "2: Strategy plan implemented, with some actions in practice.",
-                "3: Fully operational strategy plan, with regular review and updates."
-            ]
-        },
-        {
-            "name": "13.2 Emissions Inventory",
-            "options": [
-                "0: Emissions inventory not carried out.",
-                "1: Emissions inventory conducted, but outdated.",
-                "2: Emissions inventory conducted, updated more than a year ago.",
-                "3: Emissions inventory updated annually and actively used for emissions management."
-            ]
-        },
-        {
-            "name": "13.3 Carbon Credit Management Program",
-            "options": [
-                "0: No Carbon Credit Program.",
-                "1: Program in initial phase, without credits generated or purchased.",
-                "2: Active program, with carbon credits being generated or purchased.",
-                "3: Well-established program, with carbon credits being actively managed."
-            ]
-        },
-        {
-            "name": "13.4 Climate Monitoring Program",
-            "options": [
-                "0: Non-existent program.",
-                "1: Program in implementation phase.",
-                "2: Program implemented, but data used in a limited way.",
-                "3: Program implemented and integrated into a climate response and planning system."
-            ]
-        },
-        {
-            "name": "13.5 Number of Collaborations and Partnerships for Climate Action",
-            "options": [
-                "0: No collaboration or partnership established.",
-                "1: Up to 2 collaborations or partnerships established.",
-                "2: 3-5 collaborations or partnerships with initial results.",
-                "3: More than 5 collaborations or partnerships with significant and measurable impact on climate action."
-            ]
-        },
-        {
-            "name": "13.6 Climate-Resilient Infrastructure",
-            "options": [
-                "0: No infrastructure assessed as climate resilient.",
-                "1: Less than 25% of infrastructure assessed as climate resilient.",
-                "2: 25-50% of infrastructure assessed as climate resilient.",
-                "3: More than 50% of infrastructure assessed as climate-resilient and adapted."
-            ]
-        },
-        {
-            "name": "13.7 Cargo Traffic Efficiency Index",
-            "options": [
-                "0: Index not calculated.",
-                "1: Efficiency index below the industry average.",
-                "2: Efficiency index at the industry average.",
-                "3: Efficiency index above the industry average, with continuous improvements."
-            ]
-        },
-        {
-            "name": "13.8 Percentage Reduction of Emissions Through New Technology Implementation",
-            "options": [
-                "0: No emission reduction programs.",
-                "1: Less than 5% reduction in emissions through new technologies.",
-                "2: 5-10% reduction in emissions through new technologies.",
-                "3: More than 10% reduction in emissions through new technologies."
-        ]}
-    
- ]}
+"ODS 13": [
+{"name": "13.1 Status do Plano de Estratégia para Mudança Climática", "options": [
+"0: Nenhuma estratégia.",
+"1: Plano estratégico em desenvolvimento, sem ações implementadas.",
+"2: Plano estratégico implementado, com algumas ações em prática.",
+"3: Plano estratégico totalmente operacional, com revisão e atualizações regulares."
+]},
+{"name": "13.2 Inventário de Emissões", "options": [
+"0: Inventário de emissões não realizado.",
+"1: Inventário de emissões realizado, mas desatualizado.",
+"2: Inventário de emissões realizado, atualizado há mais de um ano.",
+"3: Inventário de emissões atualizado anualmente e ativamente utilizado para gestão de emissões."
+]},
+{"name": "13.3 Programa de Gestão de Créditos de Carbono", "options": [
+"0: Nenhum programa de créditos de carbono.",
+"1: Programa em fase inicial, sem créditos gerados ou adquiridos.",
+"2: Programa ativo, com créditos de carbono sendo gerados ou adquiridos.",
+"3: Programa bem estabelecido, com créditos de carbono sendo ativamente geridos."
+]},
+{"name": "13.4 Programa de Monitoramento Climático", "options": [
+"0: Programa inexistente.",
+"1: Programa em fase de implementação.",
+"2: Programa implementado, mas dados usados de forma limitada.",
+"3: Programa implementado e integrado a um sistema de resposta e planejamento climático."
+]},
+{"name": "13.5 Número de Colaborações e Parcerias para Ação Climática", "options": [
+"0: Nenhuma colaboração ou parceria estabelecida.",
+"1: Até 2 colaborações ou parcerias estabelecidas.",
+"2: 3-5 colaborações ou parcerias com resultados iniciais.",
+"3: Mais de 5 colaborações ou parcerias com impacto significativo e mensurável na ação climática."
+]},
+{"name": "13.6 Infraestrutura Resiliente ao Clima", "options": [
+"0: Nenhuma infraestrutura avaliada como resiliente ao clima.",
+"1: Menos de 25% da infraestrutura avaliada como resiliente ao clima.",
+"2: De 25-50% da infraestrutura avaliada como resiliente ao clima.",
+"3: Mais de 50% da infraestrutura avaliada como resiliente ao clima e adaptada."
+]},
+{"name": "13.7 Índice de Eficiência do Tráfego de Carga", "options": [
+"0: Índice não calculado.",
+"1: Índice de eficiência abaixo da média do setor.",
+"2: Índice de eficiência na média do setor.",
+"3: Índice de eficiência acima da média do setor, com melhorias contínuas."
+]},
+{"name": "13.8 Percentual de Redução de Emissões por Meio da Implementação de Novas Tecnologias", "options": [
+"0: Nenhum programa de redução de emissões.",
+"1: Redução de emissões inferior a 5% por meio de novas tecnologias.",
+"2: Redução de 5-10% das emissões por meio de novas tecnologias.",
+"3: Redução de mais de 10% das emissões por meio de novas tecnologias."
+]}
+
+]}
+
 
     
 # Título da aplicação
@@ -222,10 +205,10 @@ def display_ods_tab(ods_group):
 
 # Exibe as variáveis e resultados para cada aba
 with tab1:
-    display_ods_tab("SDG 7")
+    display_ods_tab("ODS 7")
 
 with tab2:
-    display_ods_tab("SDG 13")
+    display_ods_tab("ODS 13")
 
     
 # Rodapé com fonte e créditos
