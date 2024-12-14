79e8536de3b031e842d62a6a833fefd07d9934e1
Darliane <140345464+darlianecunha@users.noreply.github.com>
2024-12-14 12:42:03 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -32,19 +32,18 @@ def plot_radar_chart(scores, categories):
 variaveis = {
 "ODS 7":[
     {
-        "name": "7.1 Número de programas de conscientização em uso racional de energia",
-        "options": [
+        "nome": "7.1 Número de programas de conscientização em uso racional de energia",
+       "opções": [
             "0: Nenhum programa em operação.",
             "1: Até 2 programas em operação, atingindo até 100 pessoas.",
             "2: 3-5 programas em operação, atingindo 101-500 pessoas.",
             "3: Mais de 5 programas em operação, atingindo mais de 500 pessoas."
         ]
     },
-  
 
     {
-        "name": "7.2 Número de programas de gestão de eficiência energética",
-        "options": [
+        "nome": "7.2 Número de programas de gestão de eficiência energética",
+       "opções": [
             "0: Nenhum programa em operação.",
             "1: Pelo menos um programa, resultando em até 5% de redução no consumo de energia.",
             "2: Pelo menos um programa, resultando em uma redução maior que 5% e menor ou igual a 10% no consumo de energia.",
@@ -52,8 +51,8 @@ variaveis = {
         ]
     },
     {
-        "name": "7.3 Número de iniciativas de inovação tecnológica em eficiência energética",
-        "options": [
+        "nome": "7.3 Número de iniciativas de inovação tecnológica em eficiência energética",
+       "opções": [
             "0: Nenhuma iniciativa.",
             "1: Até 2 iniciativas tecnológicas implementadas.",
             "2: 3-5 iniciativas tecnológicas implementadas.",
@@ -61,7 +60,7 @@ variaveis = {
         ]
     },
     {
-        "name": "7.4 Percentual de energia renovável contratada e produzida em instalações portuárias",
+        "nome": "7.4 Percentual de energia renovável contratada e produzida em instalações portuárias",
         "options": [
             "0: Nenhum uso de energia renovável.",
             "1: Até 10% da energia utilizada é renovável.",
@@ -70,8 +69,8 @@ variaveis = {
         ]
     },
     {
-        "name": "7.5 Percentual de biocombustíveis em cargas elétricas e mecânicas",
-        "options": [
+        "nome": "7.5 Percentual de biocombustíveis em cargas elétricas e mecânicas",
+        "opções": [
             "0: Nenhum uso de biocombustíveis.",
             "1: Menos de 5% das cargas são operadas com biocombustíveis.",
             "2: 5% a 20% das cargas são operadas com biocombustíveis.",
@@ -79,8 +78,8 @@ variaveis = {
         ]
     },
     {
-        "name": "7.6 Número de iniciativas de inovação tecnológica em energia renovável",
-        "options": [
+        "nome": "7.6 Número de iniciativas de inovação tecnológica em energia renovável",
+        "opções": [
             "0: Nenhuma iniciativa.",
             "1: Até 2 iniciativas na fase de planejamento ou piloto.",
             "2: 3-5 iniciativas na fase de implementação com resultados preliminares.",
@@ -88,8 +87,8 @@ variaveis = {
         ]
     },
     {
-        "name": "7.7 Diversidade de fontes de energia renovável em instalações portuárias",
-        "options": [
+        "nome": "7.7 Diversidade de fontes de energia renovável em instalações portuárias",
+        "opções": [
             "0: Nenhum uso de fontes renováveis.",
             "1: Uso de 1 tipo diferente de energia renovável.",
             "2: Uso de 2 tipos diferentes de energia renovável.",
@@ -97,8 +96,8 @@ variaveis = {
         ]
     },
     {
-        "name": "7.8 Quantidade de parcerias para promoção de energia limpa",
-        "options": [
+        "nome": "7.8 Quantidade de parcerias para promoção de energia limpa",
+        "opções": [
             "0: Nenhuma parceria estabelecida.",
             "1: Até 2 parcerias estabelecidas com foco em energia limpa.",
             "2: 3-5 parcerias estabelecidas com foco em energia limpa.",
@@ -106,8 +105,8 @@ variaveis = {
         ]
     },
     {
-        "name": "7.9 Quantidade de estações de carregamento para veículos elétricos",
-        "options": [
+        "nome": "7.9 Quantidade de estações de carregamento para veículos elétricos",
+        "opções": [
             "0: Nenhuma estação de carregamento disponível.",
             "1: Até 5 estações de carregamento disponíveis.",
             "2: 6-15 estações de carregamento disponíveis.",
@@ -115,8 +114,8 @@ variaveis = {
         ]
     },
     {
-        "name": "7.10 Percentual de fornecimento de energia renovável para navios",
-        "options": [
+        "nome": "7.10 Percentual de fornecimento de energia renovável para navios",
+        "opções": [
             "0: Nenhum fornecimento de energia renovável para navios.",
             "1: Menos de 10% do fornecimento de energia para navios vem de fontes renováveis.",
             "2: Entre 10% e 30% do fornecimento de energia para navios vem de fontes renováveis.",
@@ -124,8 +123,8 @@ variaveis = {
         ]
     },
     {
-        "name": "7.11 Percentual de abastecimento com GNL",
-        "options": [
+        "nome": "7.11 Percentual de abastecimento com GNL",
+        "opções": [
             "0: Nenhum abastecimento de GNL.",
             "1: Menos de 5% do fornecimento total de combustível é com GNL.",
             "2: Entre 5% e 20% do fornecimento total de combustível é com GNL.",
@@ -143,7 +142,7 @@ variaveis = {
     },
     {
         "name": "7.13 Quantidade de navios usando energia renovável no porto",
-        "options": [
+        "opções": [
             "0: Nenhum navio utiliza energia renovável.",
             "1: Até 5% dos navios no porto utilizam energia renovável.",
             "2: 5% a 15% dos navios no porto utilizam energia renovável.",
@@ -152,7 +151,7 @@ variaveis = {
     },
     {
         "name": "7.14 Tarifas diferenciadas para navios com desempenho acima dos padrões ambientais",
-        "options": [
+        "opções": [
             "0: Nenhuma aplicação de tarifas diferenciadas.",
             "1: Até 5% de desconto nas tarifas portuárias.",
             "2: 5% a 15% de desconto nas tarifas portuárias.",
@@ -501,8 +500,8 @@ variaveis = {
 
 "ODS 13": [
     {
-        "name": "13.1 Status do Plano de Estratégia para Mudança Climática",
-        "options": [
+        "nome": "13.1 Status do Plano de Estratégia para Mudança Climática",
+        "opções": [
             "0: Nenhuma estratégia.",
             "1: Plano estratégico em desenvolvimento, sem ações implementadas.",
             "2: Plano estratégico implementado, com algumas ações em prática.",
@@ -510,8 +509,8 @@ variaveis = {
         ]
     },
     {
-        "name": "13.2 Inventário de Emissões",
-        "options": [
+        "nome": "13.2 Inventário de Emissões",
+        "opções": [
             "0: Inventário de emissões não realizado.",
             "1: Inventário de emissões realizado, mas desatualizado.",
             "2: Inventário de emissões realizado, atualizado há mais de um ano.",
@@ -519,8 +518,8 @@ variaveis = {
         ]
     },
     {
-        "name": "13.3 Programa de Gestão de Créditos de Carbono",
-        "options": [
+        "nome": "13.3 Programa de Gestão de Créditos de Carbono",
+        "opções": [
             "0: Nenhum programa de créditos de carbono.",
             "1: Programa em fase inicial, sem créditos gerados ou adquiridos.",
             "2: Programa ativo, com créditos de carbono sendo gerados ou adquiridos.",
@@ -537,8 +536,8 @@ variaveis = {
         ]
     },
     {
-        "name": "13.5 Número de Colaborações e Parcerias para Ação Climática",
-        "options": [
+        "nome": "13.5 Número de Colaborações e Parcerias para Ação Climática",
+        "opções": [
             "0: Nenhuma colaboração ou parceria estabelecida.",
             "1: Até 2 colaborações ou parcerias estabelecidas.",
             "2: 3-5 colaborações ou parcerias com resultados iniciais.",
@@ -547,7 +546,7 @@ variaveis = {
     },
     {
         "name": "13.6 Infraestrutura Resiliente ao Clima",
-        "options": [
+        "opções": [
             "0: Nenhuma infraestrutura avaliada como resiliente ao clima.",
             "1: Menos de 25% da infraestrutura avaliada como resiliente ao clima.",
             "2: De 25-50% da infraestrutura avaliada como resiliente ao clima.",
@@ -556,7 +555,7 @@ variaveis = {
     },
     {
         "name": "13.7 Índice de Eficiência do Tráfego de Carga",
-        "options": [
+        "opções": [
             "0: Índice não calculado.",
             "1: Índice de eficiência abaixo da média do setor.",
             "2: Índice de eficiência na média do setor.",
@@ -564,8 +563,8 @@ variaveis = {
         ]
     },
     {
-        "name": "13.8 Percentual de Redução de Emissões por Meio da Implementação de Novas Tecnologias",
-        "options": [
+        "nome": "13.8 Percentual de Redução de Emissões por Meio da Implementação de Novas Tecnologias",
+        "opções": [
             "0: Nenhum programa de redução de emissões.",
             "1: Redução de emissões inferior a 5% por meio de novas tecnologias.",
             "2: Redução de 5-10% das emissões por meio de novas tecnologias.",
